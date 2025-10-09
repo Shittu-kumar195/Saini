3323543d2c5c5d26c0e98d60a9bef7094eacf842
Shittu-kumar <shittukumar37@gmail.com>
2025-10-09 10:01:26 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -5,9 +5,9 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "20114039"))
 API_HASH = environ.get("API_HASH", "87297b8f3cc8fc9bbce591ad30da5896")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7847411487:AAHe37vtG4Z4EDD4o2rMqFlKi9gMvh-LUwY")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8348910447:AAGimU4RSBpAAxn-yRV0qqiPyyW8l3yGpuU")
 
-OWNER = int(environ.get("OWNER", "8172163893"))
+OWNER = int(environ.get("OWNER", "8025880585"))
 CREDIT = environ.get("CREDIT", "𝘽𝙃𝙐𝙈𝙄𝙃𝘼𝙍")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
@@ -23,6 +23,7 @@ if int(OWNER) not in AUTH_USERS:
 api_url = "http://master-api-v3.vercel.app/"
 api_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNzkxOTMzNDE5NSIsInRnX3VzZXJuYW1lIjoi4p61IFtvZmZsaW5lXSIsImlhdCI6MTczODY5MjA3N30.SXzZ1MZcvMp5sGESj0hBKSghhxJ3k1GTWoBUbivUe1I"
 # .....,.....,.......,...,.......,....., .....,.....,.......,...,.
+
 
 
 
